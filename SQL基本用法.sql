8880dda5141e398b1826179f70e5861d30bceb27
Zzz-L <34081899+Zzz-L@users.noreply.github.com>
2017-12-17 12:10:02 +0800

    Update SQL基本用法.sql

--- SQL基本用法.sql
+++ SQL基本用法.sql
@@ -40,3 +40,11 @@ select albums.name as 'album',albums.year,artists.name as 'artist' from albums j
 drop table if exists albums; --当表存在时则删除,不存在时则不进行任何操作
 create table if not exists albums (id integer primary key,name text,artist_id integer,year integer,FOREIGN KEY(artist_id) REFERENCES artist(id));
 --创建表  FOREIGN KEY 设置外键  一个表中的 FOREIGN KEY 指向另一个表中的 PRIMARY KEY
+--------------------------------limit用法-----------------------------
+select * from Customer LIMIT 10;--检索前10行数据，显示1-10条数据
+select * from Customer LIMIT 1,10;--检索从第2行开始，累加10条id记录，共显示id为2....11
+select * from Customer limit 5,10;--检索从第6行开始向前加10条数据，共显示id为6,7....15
+select * from Customer limit 6,10;--检索从第7行开始向前加10条记录，显示id为7,8...16
+--可以接受两个参数，第一个返回记录行的偏移量，第二个参数指定返回记录行的最大数目
+
+
